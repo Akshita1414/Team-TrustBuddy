f85167f183a750c13e6cb34b8d96f4dd5bdc385b
Akshita1414 <aakshita_be22@thapar.edu>
2025-07-15 04:15:48 +0530

    Made some changes with login and signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { TrustChecker } from './pages/TrustChecker';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+
+function PrivateRoute({ children }) {
+  const username = localStorage.getItem('username');
+  return username ? children : <Navigate to="/login" replace />;
+}
 
 function App() {
   return (
     <Router>
       <div className="font-sans">
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/checker" element={<TrustChecker />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<PrivateRoute><HomePage /></PrivateRoute>} />
+          <Route path="/checker" element={<PrivateRoute><TrustChecker /></PrivateRoute>} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </div>
     </Router>
